6c03c731ac6f8c2e5cf3b3b2a7963e538d27968b
brentgroves <brent.groves@gmail.com>
2024-12-17 19:01:09 -0500

    go_jwt:added fetch

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/brentgroves/go_jwt
 
 go 1.22.0
+
+require github.com/MicahParks/compatibility-keyfunc v0.14.0
+
+require (
+	github.com/auth0/go-jwt-middleware v1.0.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/form3tech-oss/jwt-go v3.2.5+incompatible // indirect
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/form3tech-oss/jwt-go"
+
+	keyfunc "github.com/MicahParks/compatibility-keyfunc"
+)
+
+func main() {
+
+	jwksURL := "https://raw.githubusercontent.com/istio/istio/release-1.24/security/tools/jwt/samples/jwks.json"
+	// Create the keyfunc options. Use an error handler that logs. Timeout the initial JWKS refresh request after 10
+	// seconds. This timeout is also used to create the initial context.Context for keyfunc.Get.
+	options := keyfunc.Options{
+		RefreshTimeout: time.Second * 10,
+		RefreshErrorHandler: func(err error) {
+			log.Printf("There was an error with the jwt.Keyfunc\nError: %s", err.Error())
+		},
+	}
+
+	// Create the JWKS from the resource at the given URL.
+	jwks, err := keyfunc.Get(jwksURL, options)
+	if err != nil {
+		log.Fatalf("Failed to create JWKS from resource at the given URL.\nError: %s", err.Error())
+	}
+
+	log.Println(k)
+
+	// Get a JWT to parse.
+	// jwtB64 := "eyJraWQiOiJlZThkNjI2ZCIsInR5cCI6IkpXVCIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJXZWlkb25nIiwiYXVkIjoiVGFzaHVhbiIsImlzcyI6Imp3a3Mtc2VydmljZS5hcHBzcG90LmNvbSIsImlhdCI6MTYzMTM2OTk1NSwianRpIjoiNDY2M2E5MTAtZWU2MC00NzcwLTgxNjktY2I3NDdiMDljZjU0In0.LwD65d5h6U_2Xco81EClMa_1WIW4xXZl8o4b7WzY_7OgPD2tNlByxvGDzP7bKYA9Gj--1mi4Q4li4CAnKJkaHRYB17baC0H5P9lKMPuA6AnChTzLafY6yf-YadA7DmakCtIl7FNcFQQL2DXmh6gS9J6TluFoCIXj83MqETbDWpL28o3XAD_05UP8VLQzH2XzyqWKi97mOuvz-GsDp9mhBYQUgN3csNXt2v2l-bUPWe19SftNej0cxddyGu06tXUtaS6K0oe0TTbaqc3hmfEiu5G0J8U6ztTUMwXkBvaknE640NPgMQJqBaey0E4u0txYgyvMvvxfwtcOrDRYqYPBnA"
+	jwtB64 := "eyJhbGciOiJSUzI1NiIsImtpZCI6IkRIRmJwb0lVcXJZOHQyenBBMnFYZkNtcjVWTzVaRXI0UnpIVV8tZW52dlEiLCJ0eXAiOiJKV1QifQ.eyJleHAiOjQ2ODU5ODk3MDAsImZvbyI6ImJhciIsImlhdCI6MTUzMjM4OTcwMCwiaXNzIjoidGVzdGluZ0BzZWN1cmUuaXN0aW8uaW8iLCJzdWIiOiJ0ZXN0aW5nQHNlY3VyZS5pc3Rpby5pbyJ9.CfNnxWP2tcnR9q0vxyxweaF3ovQYHYZl82hAUsn21bwQd9zP7c-LS9qd_vpdLG4Tn1A15NxfCjp5f7QNBUo-KC9PJqYpgGbaXhaGx7bEdFWjcwv3nZzvc7M__ZpaCERdwU7igUmJqYGBYQ51vr2njU9ZimyKkfDe3axcyiBZde7G6dabliUosJvvKOPcKIWPccCgefSj_GNfwIip3-SsFdlR7BtbVUcqR-yv-XOxJ3Uc1MI0tz3uMiiZcyPV7sNCU4KRnemRIMHVOfuvHsU60_GhGbiSFzgPTAa9WTltbnarTbxudb_YEOx12JiwYToeX0DCPb43W1tzIBxgm8NxUg"
+
+	// Parse the JWT.
+	token, err := jwt.Parse(jwtB64, jwks.KeyfuncF3T)
+	if err != nil {
+		log.Fatalf("Failed to parse the JWT.\nError: %s", err.Error())
+	}
+
+	// Check if the token is valid.
+	if !token.Valid {
+		log.Fatalf("The token is not valid.")
+	}
+	log.Println("The token is valid.")
+}
